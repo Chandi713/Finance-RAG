f8a3492b43a2ac1569b5108ff302f5b1c0d3eff2
Shah Yaksh Ajay <74231523+shahyaksh@users.noreply.github.com>
2025-07-13 16:10:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,7 @@ suffix=${suffix:0:18}
 # Set the necessary variables
 RESOURCE_GROUP="financerag-rg${suffix}"
 RESOURCE_PROVIDER="Microsoft.MachineLearningServices"
-REGIONS=("eastus" "westus" "centralus")
-RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
+REGIONS="eastus"
 WORKSPACE_NAME="financerag-mlw${suffix}"
 COMPUTE_INSTANCE="ci${suffix}"
 COMPUTE_CLUSTER="aml-cluster"
@@ -20,7 +19,7 @@ az provider register --namespace $RESOURCE_PROVIDER
 
 # Create the resource group and workspace and set to default
 echo "Create a resource group and set as default:"
-az group create --name $RESOURCE_GROUP --location $RANDOM_REGION
+az group create --name $RESOURCE_GROUP --location $REGION
 az configure --defaults group=$RESOURCE_GROUP
 
 echo "Create an Azure Machine Learning workspace:"
